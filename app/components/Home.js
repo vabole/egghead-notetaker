bc51ce25ec145c9f21d8312f20878a04056a1383
vabole <vabole@gmail.com>
2015-07-03 19:42:25 +0300

    completed second level

---
+++ app/components/Home.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Home = React.createClass({
+	render: function(){
+		<h2 className="text-center">
+			Search by GitHub Username Above
+		</h2>
+	}
+});
+
+module.exports = Home;
